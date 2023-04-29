573489866985442ddc16254a55312bbae37670ab
hagaitski <i.haga629@gmail.com>
2023-04-30 02:55:47 +0900

    不要なファイル削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,4 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import { trpc } from '@/libs/trpc'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main>
         <h2>Chat yesnow</h2>
         <button onClick={() => {
           mutate()
