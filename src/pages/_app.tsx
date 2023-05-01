60bdd98246a398059d75d48eba08581ef7806d12
hagaitski <i.haga629@gmail.com>
2023-05-02 03:41:10 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import React, { useMemo } from 'react';
 import { trpc } from "@/libs/trpc";
 import 'sanitize.css';
 import '@/global/base.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = useMemo(() => new QueryClient(),[]);
@@ -20,10 +21,18 @@ export default function App({ Component, pageProps }: AppProps) {
   ,[]);
 
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </trpc.Provider>
+    <>
+     <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      
+        <QueryClientProvider client={queryClient}>
+          
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </trpc.Provider>
+    </>
+    
   );
 }
