a00922c109e5563a487ad8443362b564995b2727
hagaitski <i.haga629@gmail.com>
2023-05-01 10:17:12 +0900

    format

--- src/server/procedures/truth/index.ts
+++ src/server/procedures/truth/index.ts
@@ -1,4 +1,3 @@
-import { openai } from "@/libs/openapi";
 import { sampleStory } from "@/sample/story";
 import { procedure } from "@/server/trpc";
 import { z } from "zod";
@@ -9,7 +8,7 @@ import { cosineSimilarity } from "./math";
 export const truth = procedure.input(z.object({
     storyId: z.string(),
     text: z.string(),
-})).mutation(async ({input}) => {
-    const [inputEmbedding,...truthEmbeddings] = await Promise.all([getEmbedding(input.text),getEmbedding(sampleStory.truth),...sampleStory.truthExamples.map(getEmbedding)])
-    return truthEmbeddings.some(truthEmbedding => cosineSimilarity(inputEmbedding,truthEmbedding) > THRESHOLD);
+})).mutation(async ({ input }) => {
+    const [inputEmbedding, ...truthEmbeddings] = await Promise.all([getEmbedding(input.text), getEmbedding(sampleStory.truth), ...sampleStory.truthExamples.map(getEmbedding)])
+    return truthEmbeddings.some(truthEmbedding => cosineSimilarity(inputEmbedding, truthEmbedding) > THRESHOLD);
 })
