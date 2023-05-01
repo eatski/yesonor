20abd3caf96b2a91b4737c52800ed2765cd49d91
hagaitski <i.haga629@gmail.com>
2023-05-02 05:04:54 +0900

    調整

--- src/features/header/index.tsx
+++ src/features/header/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./styles.module.scss";
 export const Header = () => {
     return <header className={styles.header}>
-        <h1>クタナレ</h1>
+        <h1>Chaiz</h1>
     </header>;
 }
