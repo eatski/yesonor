df42feaf53e2efedfbf77f2a2258cfa8ff12325e
hagaitski <i.haga629@gmail.com>
2023-05-05 03:44:47 +0900

    微調整

--- src/features/chat/ui/questionForm/index.tsx
+++ src/features/chat/ui/questionForm/index.tsx
@@ -15,7 +15,7 @@ export const QuestionForm: React.FC<{
     }>
         <label className={styles.formLabel}>質問をする</label>
         <div className={styles.formContent}>
-            <input className={styles.formInput} placeholder="はい/いいえで答えられる簡単な質問" onChange={(e) => {
+            <input className={styles.formInput} placeholder="はいorいいえで答えられる質問" onChange={(e) => {
                 inputRef.current = e.target.value
             }}
             />
