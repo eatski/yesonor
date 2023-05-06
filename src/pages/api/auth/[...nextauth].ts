936eca56c204d0259e49bce1482884e3b0fa3bb1
hagaitski <i.haga629@gmail.com>
2023-05-06 13:57:54 +0900

    NEXTAUTH_SECRET

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-if(!process.env.GOOGLE_ID || !process.env.GOOGLE_SECRET){
+if(!process.env.GOOGLE_ID || !process.env.GOOGLE_SECRET || !process.env.NEXTAUTH_SECRET){
   throw new Error("Google OAuth is not configured")
 }
 
@@ -9,7 +9,8 @@ export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
-  ]
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
 })
