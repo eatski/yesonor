fccb1df242f0dc53365d7f5ba714af645ddc1699
hagaitski <i.haga629@gmail.com>
2023-05-06 12:31:40 +0900

    フッターの作成

--- src/features/layout/index.tsx
+++ src/features/layout/index.tsx
@@ -14,5 +14,14 @@ export const Layout: React.FC<PropsWithChildren> = ({children}) => {
         <main className={styles.main}>
             {children}
         </main>
+        <footer className={styles.footer}>
+            <p>{texts.serviceName}</p>
+            <div className={styles.footerLinks}>
+                <a href="https://github.com/eatski/yesonor/blob/main/docs/%E5%88%A9%E7%94%A8%E8%A6%8F%E7%B4%84.md">利用規約</a>
+                <a href="https://github.com/eatski/yesonor/blob/main/docs/%E3%83%97%E3%83%A9%E3%82%A4%E3%83%90%E3%82%B7%E3%83%BC%E3%83%9D%E3%83%AA%E3%82%B7%E3%83%BC.md>">プライバシーポリシー</a>
+                <a href="https://github.com/eatski/yesonor/issues">報告</a>
+                <a href="https://twitter.com/eatski629">問い合わせ</a>
+            </div>
+        </footer>
     </>
 }
