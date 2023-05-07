1d9e588096ef9c3a657024119abfe52714542085
hagaitski <i.haga629@gmail.com>
2023-05-07 10:23:58 +0900

    タイムアウトを長めに

--- src/server/procedures/post/index.ts
+++ src/server/procedures/post/index.ts
@@ -26,11 +26,13 @@ export const post = procedure.input(storyInit).mutation(async ({ input, ctx }) =
 
   const retryable = (count: number,error?: any): Promise<void> => {
     if(count){
-      return setTimeout(300).then(() => ctx.doRevalidate(`/stories/${story.id}`).catch((e) => {
+      return setTimeout(1000).then(() => ctx.doRevalidate(`/stories/${story.id}`).catch((e) => {
         return retryable(count - 1,e)
       }))
     }
-    return Promise.reject(error ?? new Error("revalidation failed"))
+    return Promise.reject( new Error("revalidation failed",{
+      cause: error
+    }))
   }
   await retryable(10);
   return story;
