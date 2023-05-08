67165152963f4ae5165c7487f4207058cbeb8676
hagaitski <i.haga629@gmail.com>
2023-05-09 00:04:48 +0900

    sampleをyamlに

--- scripts/insertSampleStories.ts
+++ scripts/insertSampleStories.ts
@@ -1,20 +1,28 @@
-import { sample1, sample2 } from "../src/sample/story";
-import type { Story } from "../src/server/model/types";
 import { PrismaClient, Answer } from '@prisma/client';
 import dotenv from "dotenv";
+import { parseYaml } from "../src/features/storyYamlFileDrop/parseYaml";
+import { readFileSync } from "fs";
+import { resolve } from "path";
 dotenv.config();
 
 const prisma = new PrismaClient();
 
-async function insertStory(story: Story) {
-    const { questionExamples, ...rest } = story;
+async function insertStory(name: string) {
+    const story = parseYaml(readFileSync(resolve(process.cwd(), "samples",name), "utf-8"));
+    if(!story.success){
+        console.error("Failed to parse sample1.yaml",story.error);
+        return;
+    }
+    const { questionExamples, ...rest } = story.data;
     const questionExamplesInput = questionExamples.map(qe => ({
         ...qe,
-        answer: Answer[qe.answer]
+        answer: Answer[qe.answer],
+
     }));
     await prisma.story.create({
         data: {
             ...rest,
+            authorEmail: "yesonor@example.com",
             questionExamples: {
                 create: questionExamplesInput
             },
@@ -23,8 +31,9 @@ async function insertStory(story: Story) {
 }
 
 (async () => {
-    await insertStory(sample1);
-    await insertStory(sample2);
+    await insertStory("sample1.yaml");
+    await insertStory("sample2.yaml");
+    await insertStory("sample3.yaml");
     await prisma.$disconnect();
     console.info("Done.");
 })();
