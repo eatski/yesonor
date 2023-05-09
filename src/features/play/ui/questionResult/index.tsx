1672f0b10cfd9d0645727abe09e79f265c7a11f3
hagaitski <i.haga629@gmail.com>
2023-05-09 21:32:52 +0900

    play画面をいい感じに

--- src/features/play/ui/questionResult/index.tsx
+++ src/features/play/ui/questionResult/index.tsx
@@ -1,5 +1,6 @@
 import styles from "./styles.module.scss";
 import button from "@/styles/components.module.scss"
+import { AiFillRobot, AiOutlineComment, AiOutlineQuestion } from "react-icons/ai";
 
 export type Props = {
     question: string,
@@ -10,10 +11,18 @@ export type Props = {
 export const QuestionResult: React.FC<Props> = (props) => {
     return <section>
         <dl className={styles.container}>
-            <dt>あなたの質問</dt>
-            <dd className={styles.question}>{props.question}</dd>
-            <dt>クイズマスターの回答</dt>
-            <dd className={props.answer ? styles.answer : styles.answerLoading}>{props.answer || "考え中..."}</dd>
+            <div className={styles.content}>
+                <div className={styles.row}>
+                    <dt><AiOutlineComment /></dt>
+                    <dd className={styles.question}>{props.question}</dd>
+                </div>
+                <hr />
+                <div className={styles.row}>
+                    <dt><AiFillRobot /></dt>
+                    <dd className={props.answer ? styles.answer : styles.answerLoading}>{props.answer || "考え中..."}</dd>
+                </div>
+            </div>
+            
         </dl>
         <div className={styles.buttonContainer}>
             <button className={button.button} onClick={props.onAnswerButtonClicked}>
