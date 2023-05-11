0b3c207347215a6ff01e2c10ae0b1f7fb25035ce
hagaitski <i.haga629@gmail.com>
2023-05-11 22:14:56 +0900

    操作時にローディング

--- src/features/myStoryMenu/index.tsx
+++ src/features/myStoryMenu/index.tsx
@@ -10,12 +10,14 @@ export type Props = {
     published: boolean;
 }
 
-export const MyStoryMenu: React.FC<Props> = ({storyId,published}) => {
-    const {mutate} = trpc.delete.useMutation();
-    const { mutate: put } = trpc.put.useMutation();
-    const { mutate: publish } = trpc.publish.useMutation();
+export const MyStoryMenu: React.FC<Props> = ({ storyId, published }) => {
+    const del = trpc.delete.useMutation();
+    const put = trpc.put.useMutation();
+    const publish = trpc.publish.useMutation();
+    const isLoading = del.isLoading || put.isLoading || publish.isLoading;
+    const isIdling = del.isIdle && put.isIdle && publish.isIdle;
     const handleFileRead = (story: StoryInit) => {
-        put({
+        put.mutate({
             id: storyId,
             story
         }, {
@@ -25,40 +27,50 @@ export const MyStoryMenu: React.FC<Props> = ({storyId,published}) => {
         });
     };
     const router = useRouter();
-    return  <div className={styles.container}>
-        {
-            published || <p>
-                このストーリーは未公開です。
-            </p>
-        }
-        
-        <p>YAMLファイルでストーリーを修正</p>
-        <div className={styles.fileDropContainer}>
-            <YamlFileDrop onFileRead={handleFileRead} />
-        </div>
-        <div className={styles.buttons}>
-            <button className={published ? components.buttonDanger : components.buttonLink} onClick={() => {
-                mutate({
-                    id: storyId
-                },{
-                    onSuccess: () => {
-                        router.push(`/my/stories`);
-                    }
-                })
-            }}>
-                削除
-            </button>
-            {published || <button className={components.buttonDanger} onClick={() => {
-                publish({
-                    id: storyId,
-                },{
-                    onSuccess: () => {
-                        router.reload();
-                    }
-                })
-            }}>
-                公開
-            </button>}
+    return <div className={styles.container} data-loading={!isIdling}>
+        {isLoading ? <div className={styles.loader}/> : null}
+        <div className={styles.content}>
+            {
+                published || <p>
+                    このストーリーは未公開です。
+                </p>
+            }
+            <p>YAMLファイルでストーリーを修正</p>
+            <div className={styles.fileDropContainer}>
+                <YamlFileDrop onFileRead={handleFileRead} />
+            </div>
+            <div className={styles.buttons}>
+                <button 
+                    className={published ? components.buttonDanger : components.buttonLink} 
+                    onClick={() => {
+                        del.mutate({
+                            id: storyId
+                        }, {
+                            onSuccess: () => {
+                                router.push(`/my/stories`);
+                            }
+                        })
+                    }}
+                    disabled={!isIdling}
+                >
+                    削除
+                </button>
+                {published || <button 
+                    className={components.buttonDanger} 
+                    onClick={() => {
+                        publish.mutate({
+                            id: storyId,
+                        }, {
+                            onSuccess: () => {
+                                router.reload();
+                            }
+                        })
+                    }}
+                    disabled={!isIdling}
+                >
+                    公開
+                </button>}
+            </div>
         </div>
     </div>
 }
