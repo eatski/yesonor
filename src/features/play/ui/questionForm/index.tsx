5167b9e8992bb55e98c73bd7cae5746cd831bb92
hagaitski <i.haga629@gmail.com>
2023-05-13 20:35:25 +0900

    ローディング中はラベルを表示しない

--- src/features/play/ui/questionForm/index.tsx
+++ src/features/play/ui/questionForm/index.tsx
@@ -9,35 +9,41 @@ import { RequireLogin } from "@/features/requireLogin";
 export const QuestionForm: React.FC<{
     onSubmit: (text: string) => void;
     isLoading: boolean;
-}> = ({isLoading,onSubmit}) => {
+}> = ({ isLoading, onSubmit }) => {
     const [inputValue, setInputValue] = useState("");
     const session = useSession();
     return (
-        <form 
-            className={styles.form} 
+        <form
+            className={styles.form}
             onSubmit={(e) => {
                 e.preventDefault();
-                if(e.target instanceof HTMLFormElement && e.target.checkValidity()){
+                if (e.target instanceof HTMLFormElement && e.target.checkValidity()) {
                     onSubmit(inputValue);
                     setInputValue(""); // reset form input after submission
                 }
             }}
         >
-            <label className={styles.formLabel}>AIに質問をする</label>
-            {session.status === "loading" ? null : session.status === "authenticated" ? 
-            <div className={styles.formContent}>
-            <input 
-                className={styles.formInput} 
-                required 
-                placeholder="はい or いいえ で答えられる質問" 
-                value={inputValue} 
-                onChange={(e) => setInputValue(e.target.value)}
-            />
-            <button className={components.button} type="submit" disabled={isLoading}>
-                <AiOutlineSend size={"16px"} />
-            </button>
-        </div>: <RequireLogin />}
-            
+            {session.status === "loading" ? null : 
+                <>
+                    <label className={styles.formLabel}>AIに質問をする</label>
+                    {
+                        session.status === "authenticated" ?
+                        <div className={styles.formContent}>
+                            <input
+                                className={styles.formInput}
+                                required
+                                placeholder="はい or いいえ で答えられる質問"
+                                value={inputValue}
+                                onChange={(e) => setInputValue(e.target.value)}
+                            />
+                            <button className={components.button} type="submit" disabled={isLoading}>
+                                <AiOutlineSend size={"16px"} />
+                            </button>
+                        </div> : <RequireLogin />
+                    }
+                </>
+            }
+
         </form>
     );
 }
