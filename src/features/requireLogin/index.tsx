ed6d02d63cc8ba45bc389813fb9816782a58d24f
hagaitski <i.haga629@gmail.com>
2023-05-14 13:10:39 +0900

    ログイン導線を少し親切に

--- src/features/requireLogin/index.tsx
+++ src/features/requireLogin/index.tsx
@@ -2,9 +2,15 @@ import { signIn } from "next-auth/react";
 import styles from "./styles.module.scss";
 import components from "@/styles/components.module.scss";
 
-export const RequireLogin: React.FC = () => {
+export const RequireLogin: React.FC<{
+
+}> = () => {
     return <div className={styles.container}>
-        <p>ログインが必要です。</p>
+        <p>ログインをすると以下のことができるようになります。</p>
+        <ul>
+            <li>ストーリーの謎についてAIに質問</li>
+            <li>自作のストーリーを投稿</li>
+        </ul>
         <div>
             <button className={components.button} onClick={() => {
                 signIn();
