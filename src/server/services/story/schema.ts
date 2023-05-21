9dcdc1e6a905af61758ba33c48f36e05962b1740
hagaitski <i.haga629@gmail.com>
2023-05-21 14:51:16 +0900

    親切なUI

--- src/server/services/story/schema.ts
+++ src/server/services/story/schema.ts
@@ -1,19 +1,25 @@
 import { z } from "zod";
 
+const NON_EMPTY_MESSAGE = "この項目は必須です";
+
 const answer = z.enum(["True", "False", "Unknown", "Invalid"]);
 
 const questionExample = z.object({
-  question: z.string().min(1),
+  question: z.string().nonempty(NON_EMPTY_MESSAGE),
   answer,
-  supplement: z.string().min(1),
+  supplement: z.string().nonempty(NON_EMPTY_MESSAGE),
 });
 
+
 export const storyInit = z.object({
-  title: z.string().min(1),
-  quiz: z.string().min(1),
-  truth: z.string().min(1),
-  simpleTruth: z.string().min(1),
-  questionExamples: z.array(questionExample).min(1),
+  title: z.string().nonempty(NON_EMPTY_MESSAGE),
+  quiz: z.string().nonempty(NON_EMPTY_MESSAGE),
+  truth: z.string().nonempty(NON_EMPTY_MESSAGE),
+  simpleTruth: z.string().nonempty(NON_EMPTY_MESSAGE),
+  questionExamples: z.array(questionExample).min(3,"3つ以上の質問を入力してください").refine((data) => {
+    const answers = data.map((question) => question.answer);
+    return answers.includes("True") && answers.includes("False") && answers.includes("Unknown");
+  },"AIの精度を上げるため、回答がはい、いいえ、わからないをそれぞれ1つ以上入力してください"),
 });
 
 
