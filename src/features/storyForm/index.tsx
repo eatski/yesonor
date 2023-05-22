7fc37ce8637f1eeacb2b95f61f5cf1d979f85cff
hagaitski <i.haga629@gmail.com>
2023-05-23 04:36:54 +0900

    リファクタ

--- src/features/storyForm/index.tsx
+++ src/features/storyForm/index.tsx
@@ -6,6 +6,7 @@ import components from '@/styles/components.module.scss';
 import { AiFillPlayCircle, AiOutlineMinusCircle, AiOutlinePlus, AiOutlineUpload } from 'react-icons/ai';
 import { z } from 'zod';
 import { storyInit } from '@/server/services/story/schema';
+import { QuestionExampleForm } from './components/questionExample';
 
 type StoryInit = z.infer<typeof storyInit>;
 
@@ -16,7 +17,7 @@ export type Props = {
     isError: boolean,
 }
 
-export const StoryForm: React.FC<Props> = ({onSubmit,isError,isLoading,storyInit: story}) => {
+export const StoryForm: React.FC<Props> = ({ onSubmit, isError, isLoading, storyInit: story }) => {
     const { register, control, handleSubmit, formState: { errors } } = useForm<StoryInit>({
         defaultValues: story ?? {
             title: '',
@@ -51,9 +52,9 @@ export const StoryForm: React.FC<Props> = ({onSubmit,isError,isLoading,storyInit
             <div className={styles.field}>
                 <label>
                     タイトル
-                    <input 
-                        {...register('title')} 
-                        placeholder='例: 太郎さんのメガネ' 
+                    <input
+                        {...register('title')}
+                        placeholder='例: 太郎さんのメガネ'
                         className={components.input}
                     />
                     {errors.title && <p>{errors.title.message}</p>}
@@ -62,9 +63,9 @@ export const StoryForm: React.FC<Props> = ({onSubmit,isError,isLoading,storyInit
             <div className={styles.field}>
                 <label>
                     問題文
-                    <textarea 
-                        {...register('quiz')} 
-                        placeholder='例: 太郎さんは視力がとてもいいのにメガネをかけている。なぜか？' 
+                    <textarea
+                        {...register('quiz')}
+                        placeholder='例: 太郎さんは視力がとてもいいのにメガネをかけている。なぜか？'
                         className={components.textarea}
                     />
                     {errors.quiz && <p>{errors.quiz.message}</p>}
@@ -76,8 +77,8 @@ export const StoryForm: React.FC<Props> = ({onSubmit,isError,isLoading,storyInit
                     <p className={styles.description}>
                         AIは質問に対する回答を生成する際にこの文章を参照します。そのため、真相は詳細まで記述することをお勧めします。
                     </p>
-                    <textarea 
-                        {...register('truth')} 
+                    <textarea
+                        {...register('truth')}
                         placeholder='例: 太郎さんはオシャレ好きであり、おしゃれのために伊達メガネをかけている。'
                         className={components.input}
                     />
@@ -90,13 +91,13 @@ export const StoryForm: React.FC<Props> = ({onSubmit,isError,isLoading,storyInit
                     <p className={styles.description}>
                         この文章はAIが回答者の解答を判定する際に使用します。詳細に説明しすぎると、AIに回答者の解答が「情報が不足している」と判断されてしまうため、<strong>真相の核心を最低限の言葉で</strong>記述してください。
                     </p>
-                    <textarea 
+                    <textarea
                         {...register('simpleTruth')}
-                        placeholder='例: 太郎さんは伊達メガネをかけている。' 
+                        placeholder='例: 太郎さんは伊達メガネをかけている。'
                         className={components.input}
                     />
                     {errors.simpleTruth && <p>{errors.simpleTruth.message}</p>}
-                    
+
                 </label>
             </div>
             <fieldset>
@@ -110,54 +111,15 @@ export const StoryForm: React.FC<Props> = ({onSubmit,isError,isLoading,storyInit
                 }
                 {fields.map((_, index) => (
                     <div key={index} className={styles.questionExampleItem}>
-                        <div className={styles.questionExampleHead}>
-                            Q{index + 1}
-                            {index > 0 ? <button type="button" onClick={() => remove(index)}>
-                                <AiOutlineMinusCircle className={components.iconButtonDanger} />
-                            </button> : null}
-                        </div>
-                        <div className={styles.field}>
-                            <label>
-                                質問
-                                <input 
-                                    {...register(`questionExamples.${index}.question`)}
-                                    placeholder='例: 太郎さんは男ですか？' 
-                                    className={components.input}
-                                />
-                                {errors.questionExamples && errors.questionExamples[index]?.question && <p>{errors.questionExamples[index]?.question?.message}</p>}
-                            </label>
-                        </div>
-                        <div className={styles.field}>
-                            <label>
-                                回答
-                                <Controller
-                                    name={`questionExamples.${index}.answer`}
-                                    control={control}
-                                    render={({ field }) => (
-                                        <div className={components.selectWrapper}>
-                                            <select 
-                                                {...field}
-                                            >
-                                                <option value="True">はい</option>
-                                                <option value="False">いいえ</option>
-                                                <option value="Unknown">わからない（言及されてない・真相には関係ない）</option>
-                                            </select>
-                                        </div>
-                                    )}
-                                />
-                            </label>
-                        </div>
-                        <div className={styles.field}>
-                            <label>
-                                補足説明
-                                <input 
-                                    {...register(`questionExamples.${index}.supplement`)}
-                                    placeholder='例: 太郎さんの性別については言及されていません。' 
-                                    className={components.input}
-                                />
-                                {errors.questionExamples && errors.questionExamples[index]?.supplement && <p>{errors.questionExamples[index]?.supplement?.message}</p>}
-                            </label>
-                        </div>
+                        <QuestionExampleForm
+                            index={index}
+                            questionInput={register(`questionExamples.${index}.question`)}
+                            questionError={(errors.questionExamples && errors.questionExamples[index]?.question?.message) ?? null}
+                            answerInput={register(`questionExamples.${index}.answer`)}
+                            supplementInput={register(`questionExamples.${index}.supplement`)}
+                            supplementError={(errors.questionExamples && errors.questionExamples[index]?.supplement?.message) ?? null}
+                            onClickRemove={() => remove(index)}
+                        />
                     </div>
                 ))}
                 <div className={styles.questionExampleFoot}>
