0ea91e3d3f29aae09b23b06a197a6a4c4de065db
hagaitski <i.haga629@gmail.com>
2023-05-26 04:53:45 +0900

    dotenv.config()

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+dotenv.config()
 export default defineConfig({
   plugins: [react()],
   test: {
