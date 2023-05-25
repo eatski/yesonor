528bfc57bd32295e51676251a5c8eba4b9590e79
hagaitski <i.haga629@gmail.com>
2023-05-26 03:08:23 +0900

    question prcをtest

--- src/server/procedures/question/index.test.ts
+++ src/server/procedures/question/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect, beforeAll, afterAll, vi } from "vitest";
+import { describe, test, expect} from "vitest";
 import { appRouter } from "@/server";
 import { openaiForTest } from "@/libs/openai/forTest";
 const never = () => {
@@ -9,7 +9,7 @@ const getUserOptionalMock = async () => ({
     email: "aaa",
 } as const) 
 describe("trpc/question", () => {
-    test("test1",async  () => {
+    test("真相に対して正しい質問をするとTrueが返る",async  () => {
         const result = await appRouter.createCaller({
             getUserOptional: getUserOptionalMock,
             getUser: never,
@@ -22,5 +22,33 @@ describe("trpc/question", () => {
             recaptchaToken: "anytoken"
         })
         expect(result).toEqual("True");
+    })
+    test("真相に対して正しくない質問をするとFalseが返る",async  () => {
+        const result = await appRouter.createCaller({
+            getUserOptional: getUserOptionalMock,
+            getUser: never,
+            doRevalidate: never,
+            verifyRecaptcha: () => Promise.resolve(),
+            openai: openaiForTest
+        }).question({
+            storyId: "test",
+            text: "太郎のメガネには度が入っていますか？",
+            recaptchaToken: "anytoken"
+        })
+        expect(result).toEqual("False");
+    })
+    test("真相では言及されていない質問をするとUnknownが返る",async  () => {
+        const result = await appRouter.createCaller({
+            getUserOptional: getUserOptionalMock,
+            getUser: never,
+            doRevalidate: never,
+            verifyRecaptcha: () => Promise.resolve(),
+            openai: openaiForTest
+        }).question({
+            storyId: "test",
+            text: "太郎には恋人がいますか？",
+            recaptchaToken: "anytoken"
+        })
+        expect(result).toEqual("Unknown");
     })
 })
