14ffcb1934e368fb01759db4c20829a9153135bd
hagaitski <i.haga629@gmail.com>
2023-06-19 22:54:46 +0900

    文字数を減らす

--- src/features/layout/index.tsx
+++ src/features/layout/index.tsx
@@ -118,8 +118,8 @@ export const Layout: React.FC<
 				<div className={styles.footerLinks}>
 					<Link href="/terms">利用規約</Link>
 					<Link href="/privacy">プライバシーポリシー</Link>
-					<Link href="/about">当サイトについて</Link>
-					<a href="https://github.com/eatski/yesonor">ソースコード</a>
+					<Link href="/about">サイトについて</Link>
+					<a href="https://github.com/eatski/yesonor">開発</a>
 				</div>
 			</footer>
 		</>
