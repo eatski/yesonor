d41f64c1d5c6efb8192750cfe89802d0bd00383b
hagaitski <i.haga629@gmail.com>
2023-09-17 12:30:27 +0900

    modalをsupenseに

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,26 +1,32 @@
-import ReactModal from "react-modal";
+import React from "react";
+import { Suspense } from "react";
+
+const LazyReactModal = React.lazy(() => import("react-modal"));
 
 export const Modal: React.FC<React.PropsWithChildren<{ isOpen: boolean }>> = ({
 	children,
 	isOpen,
 }) => {
 	return (
-		<ReactModal
-			isOpen={isOpen}
-			style={{
-				content: {
-					width: "80%",
-					height: "fit-content",
-					margin: "auto",
-					padding: "12px",
-					backgroundColor: "var(--color-base-bg)",
-				},
-				overlay: {
-					backgroundColor: "var(--color-base-bg-overlay)",
-				},
-			}}
-		>
-			{children}
-		</ReactModal>
+		<Suspense>
+			<LazyReactModal
+				isOpen={isOpen}
+				style={{
+					content: {
+						width: "80%",
+						height: "fit-content",
+						margin: "auto",
+						padding: "12px",
+						backgroundColor: "var(--color-base-bg)",
+					},
+					overlay: {
+						backgroundColor: "var(--color-base-bg-overlay)",
+					},
+				}}
+				parentSelector={() => document.body}
+			>
+				{children}
+			</LazyReactModal>
+		</Suspense>
 	);
 };
