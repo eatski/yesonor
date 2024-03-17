5ed05d28ad4245a001cfd7c470323980757f30e0
hagaitski <i.haga629@gmail.com>
2024-03-17 19:51:21 +0900

    fallback

--- src/libs/claude/index.ts
+++ src/libs/claude/index.ts
@@ -3,17 +3,13 @@ import {
 	type MessageCreateParamsNonStreaming,
 } from "@anthropic-ai/sdk/resources";
 
-if (!process.env["ANTHROPIC_API_KEY"]) {
-	throw new Error("ANTHROPIC_API_KEY is not defined");
-}
-
 const apiKey = process.env["ANTHROPIC_API_KEY"];
 
 export const createMessage = (config: MessageCreateParamsNonStreaming) => {
 	return fetch("https://api.anthropic.com/v1/messages", {
 		headers: {
 			"Content-Type": "application/json",
-			"x-api-key": apiKey,
+			"x-api-key": apiKey || "",
 			"anthropic-version": "2023-06-01",
 		},
 		method: "POST",
