f7318de78fbbcba36065bcc83aada40cdbf52852
hagaitski <i.haga629@gmail.com>
2024-03-20 18:17:16 +0900

    dummy OPENAI_API_KEY

--- src/libs/openai/index.ts
+++ src/libs/openai/index.ts
@@ -3,4 +3,5 @@ export const openai = new OpenAI({
 	// fetchを直接使うとmswの上書きが間に合わないので、fetchのwrapperにする。
 	fetch: (...args) => fetch(...args),
 	timeout: 20000,
+	apiKey: process.env.OPENAI_API_KEY || "dummy",
 });
