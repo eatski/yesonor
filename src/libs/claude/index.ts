86b638be3593227dce7bff3803c3ece62d6e168a
hagaitski <i.haga629@gmail.com>
2024-03-20 18:57:56 +0900

    dummy

--- src/libs/claude/index.ts
+++ src/libs/claude/index.ts
@@ -2,7 +2,7 @@ import Anthropic from "@anthropic-ai/sdk";
 import { type MessageCreateParamsNonStreaming } from "@anthropic-ai/sdk/resources";
 
 const anthropic = new Anthropic({
-	apiKey: process.env["ANTHROPIC_API_KEY"],
+	apiKey: process.env["ANTHROPIC_API_KEY"] || "dummy",
 	fetch: (...args) => fetch(...args),
 	timeout: 2000,
 });
