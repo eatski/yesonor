8f8eab28da38305bbfda00e89d0ca53c5909c3c8
hagaitski <i.haga629@gmail.com>
2024-07-09 22:00:20 +0900

    revalidate

--- src/app/stories/page.tsx
+++ src/app/stories/page.tsx
@@ -1,7 +1,10 @@
+import { revalidateTime } from "@/common/revalidate";
 import { StoryList } from "@/components/storyList";
 import { H2 } from "@/designSystem/components/heading";
 import { getStories } from "@/server/services/story";
 import { Layout } from "../_components/layout";
+
+export const revalidate = revalidateTime.short;
 
 const NewerStories = async () => {
 	const stories = await getStories({
