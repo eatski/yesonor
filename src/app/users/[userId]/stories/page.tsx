ab8edd25939b5d49c150d64fe6be8e00f27f8df1
hagaitski <i.haga629@gmail.com>
2024-07-11 12:05:36 +0900

    修正

--- src/app/users/[userId]/stories/page.tsx
+++ src/app/users/[userId]/stories/page.tsx
@@ -3,6 +3,8 @@ import { StoryList } from "@/components/storyList";
 import { H1 } from "@/designSystem/components/heading";
 import { getUserSession } from "@/server/serverComponent/getUserSession";
 import { getStoriesWithAuthorId } from "@/server/services/story";
+import { getUser } from "@/server/services/user";
+import { notFound } from "next/navigation";
 import { z } from "zod";
 
 const paramsSchema = z.object({
@@ -13,24 +15,30 @@ export default async function UsersStories({ params }: { params: unknown }) {
 	const { userId: slugUserId } = paramsSchema.parse(params);
 	const user = await getUserSession();
 	const isOwn = slugUserId === user?.userId;
-	const data = await getStoriesWithAuthorId({
-		authorId: slugUserId,
-		includePrivate: isOwn,
-	});
-	const authorName = data.at(0)?.author.name;
-	const stories = data.map((story) => ({
+	const [storyData, authorName] = await Promise.all([
+		getStoriesWithAuthorId({
+			authorId: slugUserId,
+			includePrivate: isOwn,
+		}),
+		isOwn
+			? "あなた"
+			: getUser({ userId: slugUserId }).then((user) => user?.name),
+	]);
+	if (!authorName) {
+		return notFound();
+	}
+	const stories = storyData.map((story) => ({
 		story,
 		url: `/stories/${story.id}`,
 	}));
 
-	const displayName = isOwn ? "あなた" : authorName || "匿名ユーザー";
-
 	return (
 		<main>
-			<H1>{displayName}のストーリー</H1>
+			<H1>{authorName}のストーリー</H1>
 			{stories.length ? (
 				<StoryList stories={stories} />
 			) : (
+				// FIXME: 別人のストーリーを見たときに表示されるのはおかしい
 				<RecommendCreateStory />
 			)}
 		</main>
