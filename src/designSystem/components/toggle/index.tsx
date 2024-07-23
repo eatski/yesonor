80342d03424dad91302d8b791a946a928b50b859
hagaitski <i.haga629@gmail.com>
2024-07-23 12:13:15 +0900

    toggle周りの修正

--- src/designSystem/components/toggle/index.tsx
+++ src/designSystem/components/toggle/index.tsx
@@ -2,34 +2,26 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 export type ToggleButtonProps = {
+	id: string;
 	onToggle: () => void;
 	isOn: boolean;
 };
 
 export const ToggleButton: React.FC<ToggleButtonProps> = ({
+	id,
 	isOn,
 	onToggle,
 }) => {
 	return (
 		<div
+			id={id}
 			className={`${styles.toggleSwitch} ${isOn ? styles.toggleSwitchOn : ""}`}
 			onClick={onToggle}
-			role="checkbox"
+			role="switch"
 			aria-checked={isOn}
 			tabIndex={0}
 		>
 			<div className={styles.toggleSwitchSlider} />
-		</div>
-	);
-};
-
-export const ToggleWithLabel: React.FC<
-	ToggleButtonProps & { label: string }
-> = ({ label, ...props }) => {
-	return (
-		<div className={styles.toggleWithLabel}>
-			<ToggleButton {...props} />
-			<span className={styles.toggleLabel}>{label}</span>
 		</div>
 	);
 };
