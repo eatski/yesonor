c2df1aa51616bdb9f3c4c94e3e95e2901d9e72e4
hagaitski <i.haga629@gmail.com>
2024-08-11 16:36:37 +0900

    editにtitle設定

--- src/app/stories/[storyId]/edit/page.tsx
+++ src/app/stories/[storyId]/edit/page.tsx
@@ -3,23 +3,20 @@ import { EditStory } from "@/components/editStory";
 import { getUserSession } from "@/server/serverComponent/getUserSession";
 import { getStory } from "@/server/services/story";
 import { updateStory } from "@/server/services/story/updateStory";
+import { Metadata } from "next";
 import { revalidateTag } from "next/cache";
 import { headers } from "next/headers";
 import { notFound } from "next/navigation";
+import { cache } from "react";
 import { z } from "zod";
 
-const paramsSchema = z.object({
-	storyId: z.string(),
-});
-
-export default async function StoryEditPage({ params }: { params: unknown }) {
-	const { storyId } = paramsSchema.parse(params);
+const getDataByRequest = cache(async (storyId: string) => {
 	const user = await getUserSession();
 	if (!user) {
 		notFound();
 	}
 	const story = await getStory({
-		storyId,
+		storyId: storyId,
 		filter: {
 			type: "withAuthorId",
 			authorId: user.userId,
@@ -28,6 +25,33 @@ export default async function StoryEditPage({ params }: { params: unknown }) {
 	if (!story) {
 		notFound();
 	}
+	return {
+		story,
+		user,
+	};
+});
+
+const paramsSchema = z.object({
+	storyId: z.string(),
+});
+
+export const generateMetadata = async ({
+	params,
+}: { params: unknown }): Promise<Metadata> => {
+	const parsed = paramsSchema.safeParse(params);
+	if (!parsed.success) {
+		notFound();
+	}
+	const { storyId } = parsed.data;
+	const { story } = await getDataByRequest(storyId);
+	return {
+		title: `編集 - ${story.title}`,
+	};
+};
+
+export default async function StoryEditPage({ params }: { params: unknown }) {
+	const { storyId } = paramsSchema.parse(params);
+	const { story, user } = await getDataByRequest(storyId);
 	const device = getDevice(headers().get("user-agent") || undefined);
 	return (
 		<EditStory
