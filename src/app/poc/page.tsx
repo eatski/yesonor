33218b9ea990dfebc6bd89357109c290efbb9fc9
hagaitski <i.haga629@gmail.com>
2024-08-29 03:19:03 +0900

    cachedNow

--- src/app/poc/page.tsx
+++ src/app/poc/page.tsx
@@ -1,10 +1,22 @@
+import { nextCache } from "@/server/serverComponent/nextCache";
 import Link from "next/link";
 
-export default function PoC() {
+export default async function PoC() {
+	const from = new Date();
+	const cachedNow = await nextCache(() => {
+		const now = new Date();
+		console.log("nextCache", now.toISOString());
+		return now;
+	}, ["cachedNow"])();
+	const to = new Date();
 	return (
 		<div>
 			<h1>PoC</h1>
 			<p>Proof of concept.</p>
+			<p>{cachedNow}</p>
+			<p>
+				{from.toISOString()} ~ {to.toISOString()}
+			</p>
 			{[1, 2, 3].map((i) => (
 				<Link prefetch key={i} href={`/poc/${i}`}>
 					{i}
