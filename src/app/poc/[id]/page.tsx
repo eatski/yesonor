e19b509817decdb58edae6bb827040139fb08d5d
hagaitski <i.haga629@gmail.com>
2024-08-29 02:38:40 +0900

    a

--- src/app/poc/[id]/page.tsx
+++ src/app/poc/[id]/page.tsx
@@ -1,7 +1,10 @@
+import { cookies } from "next/headers";
+
 const now = new Date();
 console.log(now.toISOString());
 
 export default function PoC() {
+	cookies().get("key");
 	return (
 		<div>
 			<h1>PoC</h1>
