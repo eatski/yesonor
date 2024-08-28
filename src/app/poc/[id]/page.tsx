08cabedab53cf07f4f1ad520823a70f5efa4ff3b
hagaitski <i.haga629@gmail.com>
2024-08-29 03:28:09 +0900

    cache

--- src/app/poc/[id]/page.tsx
+++ src/app/poc/[id]/page.tsx
@@ -1,15 +1,22 @@
+import { nextCache } from "@/server/serverComponent/nextCache";
 import { cookies } from "next/headers";
 
 const now = new Date();
 console.log(now.toISOString());
 
-export default function PoC() {
+export default async function PoC() {
 	cookies().get("key");
 	console.log("render", now.toISOString());
+	const cachedNow = await nextCache(() => {
+		const now = new Date();
+		console.log("nextCache", now.toISOString());
+		return now.toISOString();
+	}, ["cachedNow2"])();
 	return (
 		<div>
 			<h1>PoC</h1>
 			<p>{now.toISOString()}</p>
+			<p>{cachedNow}</p>
 		</div>
 	);
 }
